82c35c5abd87df9fac107aaf3c71d435e1179622
Sonal Bhoraniya <bhoraniya@bhoraniya-macbookair.roam.corp.google.com>
2020-03-15 13:40:57 -0700

    added logic for everything

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devour) VALUES ('Veggie', false);
+INSERT INTO burgers (name, devour) VALUES ('Black Bean', false);
+INSERT INTO burgers (name, devour) VALUES ('Impossible', false);
+
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,46 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-devour").on("click", function(event) {
+    var id = $(this).data("id");
+   
+
+    var newDevourState = {
+      devour: 1
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("changed devour to", 1);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burger").val().trim(),
+      devour: 0
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+});
